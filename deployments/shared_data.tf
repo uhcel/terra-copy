8c52d9601f84bf13919e023f4ece456ad1053036
Adrien Walkowiak <awalko@google.com>
2019-08-07 08:27:43 -0500

    testing backend

--- deployments/shared_data.tf
+++ deployments/shared_data.tf
@@ -1,8 +1,5 @@
   data "terraform_remote_state" "networking-dev" {
   backend = "gcs"
-
-  config {
-    bucket  = "cf-tf-seed-bucket"
-    prefix  = "tf-demo/state/networking"
-  }
+  bucket  = "cf-tf-seed-bucket"
+  prefix  = "tf-demo/state/networking"
 }
